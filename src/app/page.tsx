be8c22d6299de6d9cde8b748b632042fe833581a
Ioann Kurchin <i.kurchin@bilderlings.eu>
2024-01-25 02:53:50 +0300

    Navbar for desktop created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,7 +31,7 @@ export default function Home() {
         <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
           <h1 className="font-bold tracking-tight text-gray-900 text-4xl sm:text-6xl">
             Your marketplace for high-quality{" "}
-            <span className="text-rose-600">digital assets</span>.
+            <span className="text-blue-600">digital assets</span>.
           </h1>
           <p className="mt-6 text-lg max-w-prose text-muted-foreground">
             Welcome to DigitalHippo. Every asset on our platform is verified by
@@ -56,7 +56,7 @@ export default function Home() {
                 key={perk.name}
                 className="text-center md:flex md:items-start md:text-left lg:block lg:text-center">
                 <div className="md:flex-shrink-0 flex justify-center">
-                  <div className="h-16 w-16 flex items-center justify-center rounded-full bg-rose-100 text-rose-900">
+                  <div className="h-16 w-16 flex items-center justify-center rounded-full bg-blue-100 text-blue-900">
                     {<perk.Icon className="w-1/3 h-1/3" />}
                   </div>
                 </div>
